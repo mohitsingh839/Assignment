49d3d7663bc2c00afef32188acf53e50024279af
mohitsingh839 <58594360+mohitsingh839@users.noreply.github.com>
2021-05-15 11:28:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// impoting moldules from React
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// redering the app
+ReactDOM.render(<App />, document.getElementById('root'));
